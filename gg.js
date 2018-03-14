31f75b93e692bb3cbeae2b066270e2ab110743e2
AdilsonSaldanha <saladanhadilson543@gmail.com>
2018-03-14 02:01:01 -0700

    add code in css,js,json file

--- gg.js
+++ gg.js
@@ -1,23 +1,53 @@
-function loadJSON(url){
-return new Promise(resolve, reject)=> {
-return fetch(url).them(response=>{
-if (response.ok){
-resolve(resolve.json());
-}else{
-reject(new error('error-unable to load'));
+/*
+function loadJSON(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === "200") {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send(null);
 }
-})
-})
-}
-var myPromise = loadJSON("data.json");
-myPromise.then(data=>{
-console.log(data);
+//var url = "https://jaganmohangumpa.github.io/resume/resources/data.json";
+//usage:
+loadJSON("api-data/data.json", function(text) {
+  let data = JSON.parse(text);
+  console.log(data);
 });
+*/
 
-var content=documente.querSelector(".content");
+function loadJSON(url){
+  return new Promise( (resolve, reject) => {
+    return fetch(url).then( response => {
+      if (response.ok) {
+        resolve(response.json());
+      } else {
+        reject(new Error('error - unable to load JSON'));
+      }
+    })
+  })
+}
+
+var myPromise = loadJSON("main.json");
+
+myPromise.then( data => {
+  console.log(data);
+  career(data.career);
+} );
+
+var content=document.querySelector(".content");
+
 function career(car){
-h2.textContent="Career Objective";
-content.appendChild(h2);
-var hr=document.createElement("hr");
-content.appendChild(hr);
-p.create Element
+  var h3=document.createElement("h3");
+  h3.textContent="Career Objective.";
+  content.appendChild(h3);
+
+  var hr=document.createElement("hr");
+  content.appendChild(hr);
+
+  var p=document.createElement("p");
+  p.textContent=car.data;
+  content.appendChild(p);
+}
